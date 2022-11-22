54af133f3881e70a573f2c0b37c0266f87687bec
AlphaSgh <upupsgh@163.com>
2022-11-22 23:32:35 +0800

    20221122

--- java/fristJava/src/Main.java
+++ java/fristJava/src/Main.java
@@ -1,17 +1,44 @@
 public class Main {
     public static void main(String[] args) {
+        Persion ming = new Persion();
+        ming.setAge(11);
+        ming.setName("ming");
+        System.out.println("ming age==" + ming.getAge());
+        System.out.println("ming name==" + ming.getName());
+/*
+        City bj = new City();
+        bj.name = "BeiJing";
+        bj.latitude = 39.903;
+        bj.longitude = 116.401;
+
+        System.out.println(bj.name);
+        System.out.println(bj.latitude);
+        System.out.println(bj.longitude);
+*/
+
+
+/*
+        for (String arg :
+                args) {
+            if ("-version".equals(arg)) {
+                System.out.println("v1.0");
+                break;
+            }
+        }
+*/
 /*
         char aChar = 'a';
         char enterChar = '\r';
         char ch = '\u9999';
         System.out.println(ch);
-        char zhong = '疯';
+        char zhong = 'zhong';
         int zongValue = zhong;
         System.out.println(zongValue);
         char c = 97;
         System.out.println(c);
         System.out.println("Hello world!");
 */
+/*
         float af = 3.1415f;
         System.out.println(af);
         double a = 0.0;
@@ -23,6 +50,12 @@ public class Main {
         System.out.println(6.0 / 0.0 == 55.0 / 0.0); //true
         System.out.println(-8 / a); //-Infinity
         System.out.println(0 / 0); // exception
-
+*/
+//        ForTest forTest = new ForTest();
+//        forTest.forTest();
+//        forTest.testArrayToString();
+//        forTest.testBubble();
+//        forTest.testDesc();
+//        forTest.testAverage();
     }
 }
---
+++ java/fristJava/src/TestClassAndInstance.java
@@ -0,0 +1,34 @@
+public class TestClassAndInstance {
+
+}
+class City {
+    String name;
+    double latitude;
+    double longitude;
+}
+class Persion {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0 || age > 100) {
+            throw new IllegalArgumentException("invalid age value");
+        }
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("invalid name");
+        }
+        this.name = name;
+    }
+}
